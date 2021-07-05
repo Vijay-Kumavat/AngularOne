c3e97d7da2f08e7b89027da2fc769d14dde96691
Kvm3 Coder <kumavatvijay776@gmail.com>
2021-07-05 14:50:55 +0530

    title change using constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularOne';
+  constructor() {
+    setTimeout(() => {
+      this.title="Change title"
+    }, 5000);
+  }
 }
